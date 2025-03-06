576418260ef10edb9522ea8969ff06842db4727a
ansilasharaf <ansilanachiveettil@gmail.com>
2025-03-06 10:59:13 +0530

    Add files via upload

---
+++ march/facto.sh
@@ -0,0 +1,13 @@
+echo "enter a number: "
+read num
+
+fact=1
+counter=1
+while [ $counter -le $num ]
+do
+        fact=$((fact * counter))
+        counter=$((counter + 1))
+done
+echo "factorial of $num is $fact "
+
+

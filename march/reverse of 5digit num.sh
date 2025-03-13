e6a67a4ded5cb2244e193e078910229a2ff8720e
ansilasharaf <ansilanachiveettil@gmail.com>
2025-03-13 11:23:18 +0530

    Create reverse of 5digit num.sh

---
+++ march/reverse of 5digit num.sh
@@ -0,0 +1,19 @@
+echo "Enter a 5-digit number:"
+read number
+
+
+if [[ $number =~ ^[0-9]{5}$ ]]; then
+
+    reversed=$(echo $number | rev)
+
+
+    echo "The reversed number is: $reversed"
+else
+    echo "Please enter a valid 5-digit number."
+fi
+
+output:
+
+Enter a 5-digit number:
+12345
+The reversed number is: 54321

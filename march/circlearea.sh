db294db5c03eaa66bfb08dbc47ec0104fc5bfbc4
ansilasharaf <ansilanachiveettil@gmail.com>
2025-03-13 10:58:15 +0530

    Create circlearea.sh

---
+++ march/circlearea.sh
@@ -0,0 +1,20 @@
+echo "Enter the radius of the circle : "
+
+read r
+pi=3.14
+
+area=$(echo "$pi * $r *$r" | bc)
+calculate_area() {
+        local radius=$1
+        local area=$(echo "$pi * $radius * $radius" | bc)
+        echo "The area of the circle is $area"
+}
+
+
+calculate_area $r
+
+output:
+
+Enter the radius of the circle : 
+3
+The area of the circle is 28.26

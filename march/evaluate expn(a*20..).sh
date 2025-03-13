522a417e0c2175a96f0710ed3b5a41c4e50c2c95
ansilasharaf <ansilanachiveettil@gmail.com>
2025-03-13 11:16:57 +0530

    Create evaluate expn(a*20..).sh

---
+++ march/evaluate expn(a*20..).sh
@@ -0,0 +1,21 @@
+echo "Enter the value of a:"
+read a
+echo "Enter the value of b:"
+read b
+echo "Enter the value of c:"
+read c
+echo "Enter the value of d:"
+read d
+result=$(echo "scale=2; ($a * 20 - $b * 2 + $c / $d)" | bc)
+echo "The result of the expression is: $result"
+
+output:
+Enter the value of a:
+2
+Enter the value of b:
+4
+Enter the value of c:
+5
+Enter the value of d:
+6
+The result of the expression is: 32.83
